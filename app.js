bc506b3deea3157e6b002027adbe509fbf296f0e
Magwit <magnushakon@hotmail.com>
2017-11-15 21:04:40 +0200

    pass pizza name + prize as object and make the item visible on order

--- app.js
+++ app.js
@@ -2,16 +2,27 @@ new Vue({
   el: "#vue-app",
   data: {
     pizzas: [
-      { name: "Capricciosa", prize: 3.0 },
-      { name: "Calzone", prize: 4.0 },
-      { name: "Rustic Vegan", prize: 5.5 }
+      {
+        name: "Capricciosa",
+        prize: 3.0
+      },
+      {
+        name: "Calzone",
+        prize: 4.0
+      },
+      {
+        name: "Rustic Vegan",
+        prize: 5.5
+      }
     ],
     order: []
   },
   methods: {
-    addItem: function(k, i) {
-      if (!this.order.includes(k)) {
-        this.order.push(k);
+    addItem: function(k, i, p) {
+      if (!this.order.includes({ name: k, prize: p })) {
+        this.order.push({ name: k, prize: p });
+
+        console.log("A " + k + " for the prize of " + p);
       } else {
         console.log("The " + k + " item was clicked more than once");
       }

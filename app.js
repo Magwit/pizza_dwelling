aa8474e814e9fef6d9d3f73cec0d82d99b84c5ba
Magwit <magnushakon@hotmail.com>
2017-11-23 15:01:24 +0200

    detect unique order clicks

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ new Vue({
         prize: 4.0
       },
       {
-        name: "Rustic Vegan",
+        name: "RusticVegan",
         prize: 5.5
       }
     ],
@@ -19,12 +19,31 @@ new Vue({
   },
   methods: {
     addItem: function(k, i, p) {
-      if (!this.order.includes({ name: k, prize: p })) {
+      if (!this.order.includes("name :" + k)) {
         this.order.push({ name: k, prize: p });
-
         console.log("A " + k + " for the prize of " + p);
       } else {
         console.log("The " + k + " item was clicked more than once");
+      }
+    },
+    hasVegan: function(k, i, p) {
+      // console.log(this.pizzas[0].name);
+      var self = this;
+      // check for empty array
+      if (!this.order.length) {
+        console.log("First!");
+        this.order.push({ name: k, prize: p });
+        // else check for matches in the array and push unique pizza names
+      } else {
+        this.order.forEach(function(arrayElem) {
+          // let result = "";
+          if (arrayElem.name == k) {
+            console.log("already got one " + k);
+            // and return a value
+          } else {
+            self.order.push({ name: k, prize: p });
+          }
+        });
       }
     }
   }

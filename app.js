17a0b14bb8505de40fcfd47ab93fc27e1a75afbd
Magwit <magnushakon@hotmail.com>
2017-11-27 19:07:16 +0200

    Add index on order items

--- app.js
+++ app.js
@@ -15,36 +15,38 @@ new Vue({
         prize: 5.5
       }
     ],
-    order: []
+    order: [],
+    ii: 0
   },
   methods: {
     addItem: function(k, i, p) {
-      if (!this.order.includes("name :" + k)) {
-        this.order.push({ name: k, prize: p });
-        console.log("A " + k + " for the prize of " + p);
+      if (this.order.length == 0) {
+        this.order.push({ name: k, prize: p, sum: p });
+      } else if (this.order.length > 0 && this.order[this.ii].name !== k) {
+        this.order.push({ name: k, prize: p, sum: p });
+        this.ii = this.ii + 1;
       } else {
         console.log("The " + k + " item was clicked more than once");
+        this.order[this.ii].sum += p;
       }
     },
-    hasVegan: function(k, i, p) {
-      // console.log(this.pizzas[0].name);
-      var self = this;
-      // check for empty array
-      if (!this.order.length) {
-        console.log("First!");
-        this.order.push({ name: k, prize: p });
-        // else check for matches in the array and push unique pizza names
-      } else {
-        this.order.forEach(function(arrayElem) {
-          // let result = "";
-          if (arrayElem.name == k) {
-            console.log("already got one " + k);
-            // and return a value
-          } else {
-            self.order.push({ name: k, prize: p });
-          }
-        });
-      }
+    incrementSum: function(k, i, p) {
+      // // TODO: Push condition create a index variable and set it to
+      // ii. In the else we increment by that variable..
+      // Do I need to pass the event? EVENT.. tells us
+      //   this.order.index.sum = sum + prize
+      // for Looped
+      // https://stackoverflow.com/questions/4689856/how-to-change-value-of-object-which-is-inside-an-array-using-javascript-or-jquer
+
+      // here is someone using .find.. I like it
+      // https://stackoverflow.com/a/45194601
+
+      console.log("the index on the Pizza menu array is " + i);
+      // this.order[0].name = "kebabpizza";
+      // the syntax is correct but the index is irrelevant.
+      // The manu index and the order index
+      // are not related
+      this.order.push({ name: "Ratt", prize: 10, sum: 10 });
     }
   }
 });

3672cfb8d09f8c41fecc5867c1ea96bb95bac79b
Magwit <magnushakon@hotmail.com>
2017-11-28 18:35:52 +0200

    add qty on order item

--- app.js
+++ app.js
@@ -15,52 +15,24 @@ new Vue({
         prize: 5.5
       }
     ],
-    order: [],
-    ii: 0
+    order: []
   },
   methods: {
-    addItem: function(k, i, p) {
-      if (this.order.length == 0) {
-        this.order.push({ name: k, prize: p, sum: p, orderId: i });
-      } else if (this.order.length > 0 && this.order[this.ii].orderId !== i) {
-        // find thwe order id in any
-        this.order.push({ name: k, prize: p, sum: p, orderId: i });
-      } else {
-        console.log("The " + k + " it was clicked more than once");
-        // this.order[this.oIndex + 1].sum += p;
-        //    let demogorgon = this.orders.find( (d) => {
-        // return d.value ===
-        //    } )
-      }
-      // how can something that is
-      // internal to the object such as objectIndex point to its own  object?
-      // is this where I use .find
-      // the else means ...
-    },
     findMe: function(k, i, p) {
-      // TODO: Use the idea about .find or findIndex below
+      // DONE: Use the idea about .find or findIndex below
       // https://stackoverflow.com/a/45194601
       // https://stackoverflow.com/a/41938641
 
-      // if nothing found
-      //    push
-      // else if found
-      //    target by index
-      //    increment sum
-      objIndex = this.order.findIndex(obj => obj.orderId === i);
-      console.log(objIndex);
+      let objIndex = this.order.findIndex(obj => obj.orderId === i); // assign
+      console.log("The objIndex is " + objIndex);
       if (objIndex === -1) {
-        this.order.push({ name: k, prize: p, sum: p, orderId: i });
+        this.order.push({ qty: 1, name: k, prize: p, sum: p, orderId: i });
       } else {
         console.log("We have a orderId " + i + " already");
-        this.order[objIndex].sum += p;
+        this.order[objIndex].qty++;
+        this.order[objIndex].sum *= this.order[objIndex].qty;
       }
-
-      // console.log("the index on the Pizza menu array is " + i);
-      // this.order[0].name = "kebabpizza";
-      // the syntax is correct but the index is irrelevant.
-      // The manu index and the order index
-      // are not related
     }
-  }
+  },
+  computed: {} // Order total
 });

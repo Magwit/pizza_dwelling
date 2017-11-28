3f296574f5973e199322c28b75f097d652c25712
Magwit <magnushakon@hotmail.com>
2017-11-28 16:01:39 +0200

    fix unique order items with incrementing item sum

--- app.js
+++ app.js
@@ -21,32 +21,46 @@ new Vue({
   methods: {
     addItem: function(k, i, p) {
       if (this.order.length == 0) {
-        this.order.push({ name: k, prize: p, sum: p });
-      } else if (this.order.length > 0 && this.order[this.ii].name !== k) {
-        this.order.push({ name: k, prize: p, sum: p });
-        this.ii = this.ii + 1;
+        this.order.push({ name: k, prize: p, sum: p, orderId: i });
+      } else if (this.order.length > 0 && this.order[this.ii].orderId !== i) {
+        // find thwe order id in any
+        this.order.push({ name: k, prize: p, sum: p, orderId: i });
       } else {
-        console.log("The " + k + " item was clicked more than once");
-        this.order[this.ii].sum += p;
+        console.log("The " + k + " it was clicked more than once");
+        // this.order[this.oIndex + 1].sum += p;
+        //    let demogorgon = this.orders.find( (d) => {
+        // return d.value ===
+        //    } )
       }
+      // how can something that is
+      // internal to the object such as objectIndex point to its own  object?
+      // is this where I use .find
+      // the else means ...
     },
-    incrementSum: function(k, i, p) {
-      // // TODO: Push condition create a index variable and set it to
-      // ii. In the else we increment by that variable..
-      // Do I need to pass the event? EVENT.. tells us
-      //   this.order.index.sum = sum + prize
-      // for Looped
-      // https://stackoverflow.com/questions/4689856/how-to-change-value-of-object-which-is-inside-an-array-using-javascript-or-jquer
-
-      // here is someone using .find.. I like it
+    findMe: function(k, i, p) {
+      // TODO: Use the idea about .find or findIndex below
       // https://stackoverflow.com/a/45194601
+      // https://stackoverflow.com/a/41938641
 
-      console.log("the index on the Pizza menu array is " + i);
+      // if nothing found
+      //    push
+      // else if found
+      //    target by index
+      //    increment sum
+      objIndex = this.order.findIndex(obj => obj.orderId === i);
+      console.log(objIndex);
+      if (objIndex === -1) {
+        this.order.push({ name: k, prize: p, sum: p, orderId: i });
+      } else {
+        console.log("We have a orderId " + i + " already");
+        this.order[objIndex].sum += p;
+      }
+
+      // console.log("the index on the Pizza menu array is " + i);
       // this.order[0].name = "kebabpizza";
       // the syntax is correct but the index is irrelevant.
       // The manu index and the order index
       // are not related
-      this.order.push({ name: "Ratt", prize: 10, sum: 10 });
     }
   }
 });
